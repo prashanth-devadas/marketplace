632cda31eb26c1b05266dd6d0c84091de4ae5fea
prashanth-devadas <prashanthharidas@yahoo.co.in>
2021-08-17 23:55:26 -0400

    minor changes

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -145,29 +145,29 @@ contract NFTMarket is ReentrancyGuard {
         // return listOfTokens;
     }
     
-    function fetchMyNFTs() public view returns (MarketItem[] memory) {
-        uint totalItemCount = _itemIds.current();
-        uint itemCount= 0;
-        uint currentIndex= 0;
+    // function fetchMyNFTs() public view returns (MarketItem[] memory) {
+    //     uint totalItemCount = _itemIds.current();
+    //     uint itemCount= 0;
+    //     uint currentIndex= 0;
         
-        for(uint i=0; i<totalItemCount; i++){
-            if(idToMarketItem[i+1].owner == msg.sender){
-                itemCount += 1;
-            }
-        }
+    //     for(uint i=0; i<totalItemCount; i++){
+    //         if(idToMarketItem[i+1].owner == msg.sender){
+    //             itemCount += 1;
+    //         }
+    //     }
         
-        MarketItem[] memory items = new MarketItem[](itemCount);
+    //     MarketItem[] memory items = new MarketItem[](itemCount);
         
-        for(uint i=0; i<totalItemCount; i++){
-            if(idToMarketItem[i+1].owner == msg.sender){
-                uint currentId = idToMarketItem[i+1].itemId;
-                MarketItem storage currentItem= idToMarketItem[currentId];
-                items[currentId] = currentItem;
-                currentIndex+=1;
-            }
-        }
+    //     for(uint i=0; i<totalItemCount; i++){
+    //         if(idToMarketItem[i+1].owner == msg.sender){
+    //             uint currentId = idToMarketItem[i+1].itemId;
+    //             MarketItem storage currentItem= idToMarketItem[currentId];
+    //             items[currentId] = currentItem;
+    //             currentIndex+=1;
+    //         }
+    //     }
 
-        return items;
-    }
+    //     return items;
+    // }
     
 }
